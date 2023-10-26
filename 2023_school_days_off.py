06cac59970b2b74dccc08aca18c0d22ace59d236
Adge Denkers <adge.denkers@gmail.com>
2023-10-26 09:42:34 -0400

    latest

---
+++ 2023_school_days_off.py
@@ -0,0 +1,35 @@
+from datetime import datetime, date, time, timedelta
+
+school_days_off = []
+school_days_off.append((date(2023, 11, 10), "Veterans Day (Observed)", "Full Day")),
+school_days_off.append((date(2023, 11, 22), "Thanksgiving Recess", "Full Day")),
+school_days_off.append((date(2023, 11, 23), "Thanksgiving Day", "Full Day")),
+school_days_off.append((date(2023, 11, 24), "Thanksgiving Recess", "Full Day")),
+school_days_off.append((date(2023, 12,  1), "Parent/Teacher Conferences", "Half Day")),
+school_days_off.append((date(2023, 12,  4), "Parent/Teacher Conferences", "Half Day")),
+school_days_off.append((date(2023, 12, 25), "Christmas Day", "Full Day")),
+school_days_off.append((date(2023, 12, 25), "Winter Recess", "Full Day")),
+school_days_off.append((date(2023, 12, 25), "Winter Recess", "Full Day")),
+school_days_off.append((date(2023, 12, 25), "Winter Recess", "Full Day")),
+school_days_off.append((date(2023, 12, 25), "Winter Recess", "Full Day")),
+school_days_off.append((date(2024,  1,  1), "New Year's Day", "Full Day")),
+school_days_off.append((date(2024,  1, 15), "Martin Luther King, Jr. Day", "Full Day")),
+school_days_off.append((date(2024,  2, 19), "February Break", "Full Day")),
+school_days_off.append((date(2024,  2, 20), "February Break", "Full Day")),
+school_days_off.append((date(2024,  2, 21), "February Break", "Full Day")),
+school_days_off.append((date(2024,  2, 22), "February Break", "Full Day")),
+school_days_off.append((date(2024,  2, 23), "February Break", "Full Day")),
+school_days_off.append((date(2024,  3, 18), "Vacation Day", "Full Day")),
+school_days_off.append((date(2024,  3, 22), "Parent/Teacher Conferences", "Half Day")),
+school_days_off.append((date(2024,  3, 25), "Parent/Teacher Conferences", "Half Day")),
+school_days_off.append((date(2024,  3, 29), "Good Friday", "Full Day")),
+school_days_off.append((date(2024,  4,  1), "Spring Recess", "Full Day")),
+school_days_off.append((date(2024,  4,  2), "Spring Recess", "Full Day")),
+school_days_off.append((date(2024,  4,  3), "Spring Recess", "Full Day")),
+school_days_off.append((date(2024,  4,  4), "Spring Recess", "Full Day")),
+school_days_off.append((date(2024,  4,  5), "Spring Recess", "Full Day")),
+school_days_off.append((date(2024,  5, 28), "Memorial Day", "Full Day")),
+school_days_off.append((date(2024,  6, 19), "Juneteenth", "Full Day")),
+
+school_events = []
+school_events.append(datetime(2024, 5, 21, 18, 00), "Spring Concert")
